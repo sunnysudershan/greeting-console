1b39f059a18b4e20df0e9973904fa1d0a885b512
Sunny Rampalli <sunnysudershan@gmail.com>
2023-07-02 09:56:42 +0400

    Update greet.js

--- greet.js
+++ greet.js
@@ -1,3 +1,7 @@
-module.exports = function greet(name) {
+module.exports = function greet(language, name) {
+  if (language === 'italian') {
+    return `Ciao ${name}!`;
+  } else {
     return `Hello ${name}!`;
-}
+  }
+};
